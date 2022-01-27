9e88e720c1536272bd77a98bf9a62d7bd84ebd3c
Seohyun Han <ep3sh06@gmail.com>
2022-01-27 13:17:40 +0900

    셀레니움을 이용한 크롤링

--- selenium_driver_crawling.py
+++ selenium_driver_crawling.py
@@ -23,7 +23,7 @@ import pandas as pd
 import time
 
 #추가적 설치
-from selenium.common.exceptions import NoSuchElementException #exception 구문 써서인지 오류나서 넣었다
+from selenium.common.exceptions import NoSuchElementException #오류나서 넣었다
 from selenium.webdriver.common.by import By #find_element_by 함수가 자꾸 error나서 불가피하게 find_element사용했다
 
 #세번째는 창 띄우고 주소 삽입하기
@@ -101,11 +101,128 @@ for i in range(0,5):
 cafeOfseoul=pd.DataFrame(all_data,columns=['내용','좋아요','날짜','장소'])
 cafeOfseoul.to_excel("seoulcafe.xlsx", index=False)
 
-#이상 인스타그램에서 정보를 크롤링해오는 방법이었다.
+#try-except 예외 처리구문
+#except [발생 오류 [as 오류 메시지 변수]]:
+
 
 #another
+#for문 대신 while문 이용하기
+
+#앞부분은 동일
+driver=webdriver.Chrome(ChromeDriverManager().install())
+url="https://www.instagram.com"
+driver.get(url)
+
+email="내아이디"
+input_id=driver.find_elements_by_css_selector("input._2hvTZ.pexuQ.zyHYP")[0]
+input_id.send_keys(email)
+password='내비밀번호'
+input_pw=driver.find_elements_by_css_selector("input._2hvTZ.pexuQ.zyHYP")[1]
+input_pw.send_keys(password)
+input_pw.submit() #입력
+time.sleep(2) #대기
+
+word=input("검색어: ") #원하는 검색어 입력
+driver.get("https://www.instagram.com/explore/tags/"+word)
+time.sleep(5)
+#첫번째 게시글 열기
+first=driver.find_element_by_css_selector("div._9AhH0")
+first.click()
+time.sleep(2)
+
+insta_dict = {'id':[],
+            'date':[],
+            'like':[],
+            'text':[],
+            'hashtag':[]}
+
+#.find_element_by_css_name VS .find_elements_by_css_name
+#단수와 복수 차이를 이해해야한다.
+#동일한 css값이 있다면 복수의 함수를 이용해야 한다.(find_elements_)
+#단 요소값이 여러개이므로 리스트 형태로 반환됨에 유의해야 한다. 리스트 형태를 바로 사용하기는 여러 error가 발생할 수 있기에
+#인덱스 추출 방식으로 이용한다.
+
+seq = 0
+start = time.time()
+
+while True:
+    try:
+        if driver.find_element_by_css_selector('a._65Bje.coreSpriteRightPaginationArrow'):
+            if seq % 20 == 0:
+                print('{}번째 수집 중'.format(seq), time.time() - start, sep='\t')
+
+            ## id 정보 수집
+            try:
+                info_id = driver.find_element_by_css_selector('h2._6lAjh').text
+                insta_dict['id'].append(info_id)
+            except:
+                info_id = driver.find_element_by_css_selector('div.C4VMK').text.split()[0]
+                insta_dict['id'].append(info_id)
+
+            ## 시간정보 수집
+            time_raw = driver.find_element_by_css_selector('time.FH9sR.Nzb55')
+            time_info = pd.to_datetime(time_raw.get_attribute('datetime')).normalize()
+            insta_dict['date'].append(time_info)
+
+            ## like 정보 수집
+            try:
+                driver.find_element_by_css_selector('button.sqdOP.yWX7d._8A5w5')
+                like = driver.find_element_by_css_selector('button.sqdOP.yWX7d._8A5w5').text
+                insta_dict['like'].append(like)
+
+            except:
+                insta_dict['like'].append('영상')
+
+            ##text 정보수집
+            raw_info = driver.find_element_by_css_selector('div.C4VMK').text.split()
+            text = []
+            for i in range(len(raw_info)):
+                ## 첫번째 text는 아이디니까 제외
+                if i == 0:
+                    pass
+                ## 두번째부터 시작
+                else:
+                    if '#' in raw_info[i]:
+                        pass
+                    else:
+                        text.append(raw_info[i])
+            clean_text = ' '.join(text)
+            insta_dict['text'].append(clean_text)
+
+            ##hashtag 수집
+            raw_tags = driver.find_elements_by_css_selector('a.xil3i')
+            hash_tag = []
+            for i in range(len(raw_tags)):
+                if raw_tags[i].text == '':
+                    pass
+                else:
+                    hash_tag.append(raw_tags[i].text)
+
+            insta_dict['hashtag'].append(hash_tag)
+
+            seq += 1
+
+            if seq == 100:
+                break
+
+            driver.find_element_by_css_selector('a._65Bje.coreSpriteRightPaginationArrow').click()
+            time.sleep(1.5)
 
 
+        else:
+            break
+
+        except:
+        driver.find_element_by_css_selector('a._65Bje.coreSpriteRightPaginationArrow').click()
+        time.sleep(2)
 
 
+#코드의 논리 핵심은 try-except이다
+#dictionary 형태에서 dataframe형태로 전환 시 가장 중요한 값은 키에서 저장하는 밸류 값들의 길이가 모두 같아야 한다는 것
+#그래서 원하는 정보가 없더라도 그냥 패스하면 안되고, null값 지정하거나 별도로 특정 값을 부여하여 value의 길이를 맞춰줘야 한다
+#크롤링을 할 때 가장 중요한 것은
+#지금 어느 정도의 단계인지 표시하고 성능을 테스트하는 것이다.
+#안 할 시 로딩시간이 매우 길어질 수 있다.
 
+#딕셔너리를 데이터프레임 형태로 정리하기
+#pd.DataFrame.from_dict()
